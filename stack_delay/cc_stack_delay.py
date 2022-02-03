162e77a8c0c009c068cb5b7b46fdaed549975d28
ekarasozen <56058741+ekarasozen@users.noreply.github.com>
2022-02-03 00:45:24 -0700

    stack & delay method station list bug fixed

--- stack_delay/cc_stack_delay.py
+++ stack_delay/cc_stack_delay.py
@@ -21,39 +21,45 @@ from obspy.signal.cross_correlation import xcorr_max
 name = "taan_fjord_waveforms_cc"
 s1 = UTCDateTime("2015-10-18 05:19:00") #  Taan Fjord
 evlat=60.175
-evlon=-141.87
-vel = 3.5 
+evlon=-141.187
+vel = 4.0
 distance = np.empty((0, 100))
 traveltime = np.empty((0,100))
 endtime = 60*7
-net= "AK,AV"
+net= "AK"
 chn = "BHZ"
-stn_list = ['MESA','YAH','RKAV','CYK','BAGL','BARK','KULT'] 
+#stn_list = ['MESA','YAH','RKAV','CYK','BAGL','BARK','KULT'] 
 st = Stream() #initial stream
 st_stack = Stream() #stream to stack
 
 #In Python, load vertical component waveforms for the event, filter them, 
 
-inv = client.get_stations(network = net, station="MESA,YAH,RKAV,CYK,BAG,BARK,KULT,TABL", starttime=s1, endtime=s1+endtime)
-print(inv)
+inv = client.get_stations(network = net, station="MESA,YAH,RKAV,CYK,BAGL,BARK,KULT,TABL,SAMH,PIN,GRNC,LOGN,BARK,ISLE,KULT,CTG,BCP,SSP,WAX,BARN,KIAG,TGL", starttime=s1, endtime=s1+endtime)
+print(inv[0])
 net = inv[0]
+#print(net[0].code)
 nos = len(net)
+print(nos)
 for s in range(nos):
-   tr = client.get_waveforms("AK", stn_list[s], "*", "BHZ", s1, s1+endtime, attach_response=True)
+   tr = client.get_waveforms("AK", net[s].code, "*", "BHZ", s1, s1+endtime, attach_response=True)
+   print(tr)
    st += tr
-   print(st)
+  # print(st)
    stalat=net[s].latitude
    stalon=net[s].longitude
+   print(net[s],stalat,stalon)
    dist=client_distaz.distaz(stalat,stalon,evlat,evlon) #calculate the distance from the source to each of the stations
    dist=dist['distancemeters']/1000
+   print(dist)
    distance = np.append(distance, [dist])
    tt = np.divide(float(dist),float(vel))
    traveltime = np.append(traveltime, [tt])
-   #print(tt)
+   print(tt)
    s2=s1+tt
-   trs = client.get_waveforms("AK", stn_list[s], "*", "BHZ", s2, s2+endtime, attach_response=True)
+   trs = client.get_waveforms("AK", net[s].code, "*", "BHZ", s2, s2+endtime, attach_response=True)
+   print(trs)
    st_stack += trs
-   print(st_stack)
+  # print(st_stack)
 
 #remove instrument response (and possibly integrate to displacement?)
 st.sort(keys=['starttime'])
@@ -61,52 +67,52 @@ st.detrend("linear")
 st.detrend("demean")
 st.taper(max_percentage=0.05, type='cosine')
 st.filter('bandpass', freqmin=0.01, freqmax=0.05)
-#st.remove_response(output='DISP')
-print(st)
+st.remove_response(output='DISP')
+#print(st)
 
-fig1 = plt.figure(figsize=(8,8))
-
-for i in range(nos):
-    ax1 = fig1.add_subplot(nos, 1,i+1)
-    tr = st[i]
-    ax1.plot(tr.times("matplotlib"), tr.data, 'k', linewidth=0.2)
-    text = (tr.stats.station + ' ' + tr.stats.channel)
-    ax1.text(0.85, 0.90, text, transform=ax1.transAxes, fontsize=10, fontweight='bold', color='blue', verticalalignment='top')
-    ax1.xaxis_date()
-    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
-    ax1.set_ylim(ymin=-250, ymax=250)
-fig1.autofmt_xdate()
-plt.tight_layout()
-plt.draw()
-fig1.autofmt_xdate()
-fig1.savefig('before_tt_correction.png')
+#fig1 = plt.figure(figsize=(8,16))
+#
+#for i in range(nos):
+#    ax1 = fig1.add_subplot(nos, 1,i+1)
+#    tr = st[i]
+#    ax1.plot(tr.times("matplotlib"), tr.data, 'k', linewidth=0.2)
+#    text = (tr.stats.station + ' ' + tr.stats.channel)
+#    ax1.text(0.85, 0.90, text, transform=ax1.transAxes, fontsize=10, fontweight='bold', color='blue', verticalalignment='top')
+#    ax1.xaxis_date()
+#    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+#    ax1.set_ylim(ymin=-250, ymax=250)
+#fig1.autofmt_xdate()
+#plt.tight_layout()
+#plt.draw()
+#fig1.autofmt_xdate()
+#fig1.savefig('before_tt_correction.png')
 
 st_stack.detrend("linear")
 st_stack.detrend("demean")
 st_stack.taper(max_percentage=0.05, type='cosine')
 st_stack.filter('bandpass', freqmin=0.01, freqmax=0.05)
-#st_stack.remove_response(output='DISP')
+st_stack.remove_response(output='DISP')
 st_stack.sort(keys=['starttime']) #not sure if this is needed?
 print(st_stack)
 
 
-fig2 = plt.figure(figsize=(8,8))
-
-for i in range(nos):
-    ax2 = fig2.add_subplot(nos, 1,i+1)
-    tr = st_stack[i]
-    ax2.plot(tr.times("matplotlib"), tr.data, 'k', linewidth=0.2)
-    text = (tr.stats.station + ' ' + tr.stats.channel)
-    ax2.text(0.85, 0.90, text, transform=ax2.transAxes, fontsize=10, fontweight='bold', color='blue', verticalalignment='top')
-    ax2.xaxis_date()
-    ax2.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
-    ax2.set_ylim(ymin=-250, ymax=250)
-fig2.autofmt_xdate()
-plt.tight_layout()
-plt.draw()
-fig2.autofmt_xdate()
-fig2.savefig('after_tt_correction.png')
-
+#fig2 = plt.figure(figsize=(8,16))
+#
+#for i in range(nos):
+#    ax2 = fig2.add_subplot(nos, 1,i+1)
+#    tr = st_stack[i]
+#    ax2.plot(tr.times("matplotlib"), tr.data, 'k', linewidth=0.2)
+#    text = (tr.stats.station + ' ' + tr.stats.channel)
+#    ax2.text(0.85, 0.90, text, transform=ax2.transAxes, fontsize=10, fontweight='bold', color='blue', verticalalignment='top')
+#    ax2.xaxis_date()
+#    ax2.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+#    ax2.set_ylim(ymin=-250, ymax=250)
+#fig2.autofmt_xdate()
+#plt.tight_layout()
+#plt.draw()
+#fig2.autofmt_xdate()
+#fig2.savefig('after_tt_correction_4.0kmsec.png')
+#
 
 st_stack.normalize()
 st_new = st_stack.stack()
@@ -130,4 +136,4 @@ ax3.text(0.85, 0.90, text, transform=ax3.transAxes, fontsize=10, fontweight='bol
 #ax1.set_ylim(ymin=ylimits[0], ymax=ylimits[1])
 #fig2.autofmt_xdate()
 
-fig3.savefig('stack.png')
+fig3.savefig('stack_4.0kmsec_disp.png')
