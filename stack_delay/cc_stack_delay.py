df99ecaf6c091ea6f57ef60144fb12c8ebb0cc90
ekarasozen <56058741+ekarasozen@users.noreply.github.com>
2022-02-04 17:02:52 -0700

    new runs with different velocities

--- stack_delay/cc_stack_delay.py
+++ stack_delay/cc_stack_delay.py
@@ -20,9 +20,11 @@ from obspy.signal.cross_correlation import xcorr_max
 
 name = "taan_fjord_waveforms_cc"
 s1 = UTCDateTime("2015-10-18 05:19:00") #  Taan Fjord
-evlat=60.175
-evlon=-141.187
-vel = 4.0
+#evlat=60.175
+#evlon=-141.187
+evlat=70
+evlon=-147
+vel = 3.5
 distance = np.empty((0, 100))
 traveltime = np.empty((0,100))
 endtime = 60*7
@@ -30,7 +32,7 @@ net= "AK"
 chn = "BHZ"
 #stn_list = ['MESA','YAH','RKAV','CYK','BAGL','BARK','KULT'] 
 st = Stream() #initial stream
-st_stack = Stream() #stream to stack
+st_corr = Stream() #stream to stack
 
 #In Python, load vertical component waveforms for the event, filter them, 
 
@@ -58,8 +60,8 @@ for s in range(nos):
    s2=s1+tt
    trs = client.get_waveforms("AK", net[s].code, "*", "BHZ", s2, s2+endtime, attach_response=True)
    print(trs)
-   st_stack += trs
-  # print(st_stack)
+   st_corr += trs
+  # print(st_corr)
 
 #remove instrument response (and possibly integrate to displacement?)
 st.sort(keys=['starttime'])
@@ -87,20 +89,20 @@ st.remove_response(output='DISP')
 #fig1.autofmt_xdate()
 #fig1.savefig('before_tt_correction.png')
 
-st_stack.detrend("linear")
-st_stack.detrend("demean")
-st_stack.taper(max_percentage=0.05, type='cosine')
-st_stack.filter('bandpass', freqmin=0.01, freqmax=0.05)
-st_stack.remove_response(output='DISP')
-st_stack.sort(keys=['starttime']) #not sure if this is needed?
-print(st_stack)
+st_corr.detrend("linear")
+st_corr.detrend("demean")
+st_corr.taper(max_percentage=0.05, type='cosine')
+st_corr.filter('bandpass', freqmin=0.01, freqmax=0.05)
+st_corr.remove_response(output='DISP')
+st_corr.sort(keys=['starttime']) #not sure if this is needed?
+print(st_corr)
 
 
 #fig2 = plt.figure(figsize=(8,16))
 #
 #for i in range(nos):
 #    ax2 = fig2.add_subplot(nos, 1,i+1)
-#    tr = st_stack[i]
+#    tr = st_corr[i]
 #    ax2.plot(tr.times("matplotlib"), tr.data, 'k', linewidth=0.2)
 #    text = (tr.stats.station + ' ' + tr.stats.channel)
 #    ax2.text(0.85, 0.90, text, transform=ax2.transAxes, fontsize=10, fontweight='bold', color='blue', verticalalignment='top')
@@ -111,20 +113,20 @@ print(st_stack)
 #plt.tight_layout()
 #plt.draw()
 #fig2.autofmt_xdate()
-#fig2.savefig('after_tt_correction_4.0kmsec.png')
+#fig2.savefig('after_tt_correction_3.5kmsec.png')
 #
 
-st_stack.normalize()
-st_new = st_stack.stack()
+st_corr.normalize()
+st_stack = st_corr.stack()
 gs = gridspec.GridSpec(2, 1)
 gs.update(wspace=0.01, hspace=0.50) # set the spacing between axes. 
 fig3 = plt.figure(figsize=(8, 8))
 
 ax3 = fig3.add_subplot(gs[0])
-ax3.plot(st_new[0].times(), st_new[0].data, "k-", linewidth=0.8, label='stream')
+ax3.plot(st_stack[0].times(), st_stack[0].data, "k-", linewidth=0.8, label='stream')
 text = (str(vel) + 'km/sec')
 ax3.text(0.85, 0.90, text, transform=ax3.transAxes, fontsize=10, fontweight='bold', color='blue', verticalalignment='top')
-
+st_stack.write("temp_stack.mseed")  
 #ax2.set_xlim(xmin=0, xmax=streamend) 
 #ax1.set_ylim(ymin=-500, ymax=500)
 #ax2.text(0.88, 0.95, text, transform=ax1.transAxes, fontsize=8, fontweight='bold', verticalalignment='top')
@@ -136,4 +138,4 @@ ax3.text(0.85, 0.90, text, transform=ax3.transAxes, fontsize=10, fontweight='bol
 #ax1.set_ylim(ymin=ylimits[0], ymax=ylimits[1])
 #fig2.autofmt_xdate()
 
-fig3.savefig('stack_4.0kmsec_disp.png')
+fig3.savefig('temp_stack_3.5kmsec_disp.png')
--- waveform_plotting/plot_waveforms_single_event.py
+++ waveform_plotting/plot_waveforms_single_event.py
@@ -10,22 +10,27 @@ import numpy as np
 
 #t1 = UTCDateTime("2020-10-05 05:02:40") # 2020 October landslide 
 #t2 = UTCDateTime("2021-08-09 07:45:40") # 2021 August landslide
-t1 = UTCDateTime("2022-01-08 16:26:00") #test event
+#t1 = UTCDateTime("2022-01-08 16:26:00") #test event
+t1 = UTCDateTime("2017-06-17 23:39:00") #test event
+t2 = UTCDateTime("2017-06-18 00:39:00") #test event
 
 #ls3 = UTCDateTime("2021-10-26 08:04:00") # 2021 October rockfall
 #ls4 = UTCDateTime("2021-10-17 21:19") # 2020 October 2nd landslide - not confirmed
 
 starttime = 60*1
-endtime = 60*35
-net= "AK"
-stn = "BAE"
-chn = "BHZ"
+endtime = 60*60
+#net= "AK"
+#stn = "BAE"
+#chn = "BHZ"
+net= "DK"
+stn = "NUUG"
+chn = "BH*"
 
 
-st = client.get_waveforms(net, stn, "*", "BH*", t1-starttime, t1+endtime, attach_response=True)
+st = client.get_waveforms(net, stn, "*", "HH*", starttime=t1, endtime=t2, attach_response=True)
 
 print(st)
-print(st[0].stats)
+print(st[0].stats.endtime)
 
 st.detrend("linear")
 st.detrend("demean")
@@ -44,7 +49,7 @@ for s in range(nos):
     ax1 = fig1.add_subplot(nos, 1,s+1)
     ax1.plot(tr.times("matplotlib"), tr.data, "k-", linewidth=0.8)
     ax1.set_xlim(timevector[0], timevector[-1]) 
-    ax1.set_ylim(ymin=-500, ymax=500)
+  #  ax1.set_ylim(ymin=-500, ymax=500)
     ax1.text(0.88, 0.95, text, transform=ax1.transAxes, fontsize=8, fontweight='bold', verticalalignment='top')
     ax1.set_ylabel('Counts')
 #   ax1.axvline(starttime, lw=0.8, c='darkblue', ls='--', label='event onset')
@@ -59,6 +64,6 @@ ax1.set_xlabel('Time (s)')
 ##ax1.set_ylim(ymin=ylimits[0], ymax=ylimits[1])
 fig1.autofmt_xdate()
 #
-fig1.savefig('jan2022_event.png', bbox_inches='tight')
+fig1.savefig('nuuk_event.png', bbox_inches='tight')
 #fig2.savefig('landslide_waveforms_abs_bae.png', bbox_inches='tight')
 #
