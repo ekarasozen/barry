5ed030cd41135bf83eb479731bc1c46cfc9eda5e
ekarasozen <56058741+ekarasozen@users.noreply.github.com>
2022-06-03 13:41:29 -0600

    big update, semblance equations are integrated. location methods are defined in locmethod

--- grid_search/codes_uptoddate/rungrid.py
+++ grid_search/codes_uptoddate/rungrid.py
@@ -3,7 +3,7 @@ import grid_search_functions as grid
 import plotgrid as plot
 import plotrecordsec as rec
 
-#grid.makestalist("AK","BAE,BAW,FID,GHO,GLI,KNK,M23K,PWL,RC01,SAW,SCM,VMT","BHZ","*","2020-10-05 05:01:00") 
+#grid.makestalist("AK","KNK,KNK,KNK","BHZ","*","2020-10-05 05:01:00") 
 #grid.creategrid(-150.0,-147.0,0.1,59,62,0.1,3.5,"stafile.csv")
 #grid.prepwaveforms("stafile.csv","2020-10-05 05:01:00",360)  
 #grid.locate("ttgridfile.npy","before_ts_wfs.mseed","lonlatgridfile.npy",3)                 
