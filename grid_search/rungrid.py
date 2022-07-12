3d702ab640a24770b500c57dc22f5397992f6923
ekarasozen <56058741+ekarasozen@users.noreply.github.com>
2022-07-12 17:50:10 +0300

    clean github

--- grid_search/rungrid.py
+++ grid_search/rungrid.py
@@ -1,10 +1,9 @@
 import os
 import grid_search_functions as grid
 import plotgrid as plot
-import plotrecordsec as rec
 
-#grid.makestalist("AK","BAE,BAW,FID,GHO,GLI,KNK,M23K,PWL,RC01,SAW,SCM,VMT","BHZ","*","2020-10-05 05:01:00") 
-#grid.creategrid(-150.0,-147.0,0.1,59,62,0.1,3.5,"stafile.csv")
-#grid.prepwaveforms("stafile.csv","2020-10-05 05:01:00",360)  
-#grid.locate("ttgridfile.npy","before_ts_wfs.mseed","lonlatgridfile.npy",5)                 
-plot.plotGrid("lonlatgridfile.npy","strengthfile.npy") #might move this into grid_functions
+#grid.makestalist("AK","BAE,BAW,DIV,EYAK,FID,GHO,GLI,HIN,KLU,KNK,M23K,P23K,PS12,PWL,RC01,SAW,SCM,SLK,SSN,SWD,VMT,WAT6","BHZ","*","2020-10-05 05:01:50") 
+#grid.creategrid(-150.0,-146.0,0.1,60,62,0.05,3.5,"stafile.csv")
+#grid.prepwaveforms("stafile.csv","2020-10-05 05:01:50",180)  
+grid.locate("ttgridfile.npy","before_ts_wfs.mseed","lonlatgridfile.npy",5)
+#plot.plotGrid("lonlatgridfile.npy","strengthfile.npy") #might move this into grid_functions
